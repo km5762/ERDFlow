c12ed22c3f6bed8da4b0a012342428d5f49fa29d
km5762 <km5762@DESKTOP-587P2TU>
2023-01-22 11:12:11 -0600

    Renamed files, added basic UI skeleton

---
+++ src/main/java/com/example/erdflow/ERDFlowController.java
@@ -0,0 +1,21 @@
+package com.example.erdflow;
+
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class ERDFlowController {
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+
+}
